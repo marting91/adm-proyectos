7ec5f05598446dd51c15dd9ade2b1d4e1db29019
Martin Gonzalez <gonzalez.martin.mag@gmail.com>
2021-06-20 03:48:14 -0300

    Carpeta controllers

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+const proyectosController = require('../controllers/proyectosController');
+
+
 module.exports = function () {
 
     // Ruta para el home
-    router.get('/', (req, res) => {
-        res.send("Index");
-    });
+    router.get('/', proyectosController.proyectosHome);
     router.use('/nosotros', (req, res) => {
         res.send('Nosotros');
     })
-    return router;
+    return router
 }
