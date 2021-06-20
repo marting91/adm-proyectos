07604ccbdb32ad1b216de7652d36d39c620e3ee8
Martin Gonzalez <gonzalez.martin.mag@gmail.com>
2021-06-20 03:22:43 -0300

    Commit inicial

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+// Crear una "aplicacion" de express
+const app = express();
+
+// Ruta para el home
+app.use('/', (req, res) => {
+
+});
+
+app.listen(3000);
